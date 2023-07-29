95a4d49ca3f3f9ecd48f09a803149c554b73146a
Lee Jae-yeol <rijgndqw012@gmail.com>
2023-07-29 10:53:35 +0900

    Implement `analyze` method for Analyzer (#17)
    * Add dependency for `indoc!` macro
    * Implement `analyze` method for Analyzer

--- src/analyzer.rs
+++ src/analyzer.rs
@@ -1,8 +1,10 @@
 use std::cell::{RefCell,Ref};
+use std::collections::VecDeque;
 
-use tree_sitter::{Tree, Parser, Node};
+use tree_sitter::{Tree, Parser, Node, Point, Range};
 
 use crate::grammar::get_language;
+use crate::tree_sitter_extended::{MembershipCheck, RangeFactory};
 
 pub struct Analyzer {
     pub source_code: String
@@ -10,7 +12,7 @@ pub struct Analyzer {
 
 pub trait Traversable<'a, 'b> {
     fn get_annotation_whitelist(&self) -> Vec<&str>;
-    fn analyze(&'b self);
+    fn analyze(&'b self) -> VecDeque<&'b str>;
     fn get_syntax_tree(&'b self) -> Tree;
     fn get_top_level_nodes(&'a self, tree: &'b Tree) -> Vec<Node<'b>>;
 }
@@ -54,11 +56,69 @@ impl<'a, 'b> Traversable<'a, 'b> for Analyzer {
         }
     } 
 
-    fn analyze(&'b self) {
+    fn analyze(&'b self) -> VecDeque<&'b str> {
         let tree = self.get_syntax_tree();
         let nodes = self.get_top_level_nodes(
             &tree
         );
+
+        let whitelist = self.get_annotation_whitelist();
+
+        let writer_queue: &mut VecDeque<&str> = &mut VecDeque::from([]);
+        let pending_queue: &mut VecDeque<&str> = &mut VecDeque::from([]);
+        let nodes_queue: &mut VecDeque<Node> = &mut VecDeque::from(nodes);
+        
+        for (i, line) in self.source_code.lines().enumerate() {
+            let row    = i;
+            let column = line.len();
+
+            let cursor_position = Point {
+                row,
+                column,
+            };
+
+            if nodes_queue.is_empty() {
+                writer_queue.push_back(line);
+                continue;
+            }
+
+            let current_node = match nodes_queue.front() {
+                Some(node) => node,
+                None => panic!("Failed to retrieve treesitter node from queue")
+            };
+
+            let comment_line = "/// [TODO]";
+            match Range::from_node(*current_node) {
+                node_range if cursor_position.is_member_of(node_range) => {
+                    let node_type = &current_node.kind();
+                    if whitelist.contains(node_type) {
+                        if node_type == &"attribute_item" {
+                            pending_queue.push_back(line);
+                        } else {
+                            if !pending_queue.is_empty() {
+                                writer_queue.push_back(comment_line);
+                                while !pending_queue.is_empty() {
+                                    if let Some(queued_line) = pending_queue.pop_front() {
+                                        writer_queue.push_back(queued_line);
+                                    }
+                                }
+                            }
+                            writer_queue.push_back(line);
+                        }
+                    } else {
+                        writer_queue.push_back(line);
+                    }
+                    if cursor_position == current_node.end_position() {
+                        nodes_queue.pop_front();
+                    }
+                },
+                _ => {
+                    writer_queue.push_back(line);
+                }
+            }
+        }
+
+        writer_queue.to_owned()
     }
 
     fn get_top_level_nodes(
---
+++ tests/analyzer_test/analyze_test.rs
@@ -0,0 +1,66 @@
+
+#[cfg(test)]
+mod analyze_test {
+    use indoc::indoc;
+    use balpan::analyzer::{Analyzer,Traversable};
+    use balpan::grammar::{fetch_grammars, build_grammars};
+
+    fn assert_analyzed_source_code(source_code: &str, expected: &str) {
+        fetch_grammars();
+        build_grammars(None);
+
+        let analyzer = Analyzer {
+            source_code: source_code.to_string()
+        };
+
+        let writer_queue = &analyzer.analyze();
+        let mut string_vector = vec![];
+
+        for line in writer_queue {
+            string_vector.push(*line);
+        }
+
+
+        let actual: String = string_vector
+            .iter()
+            .map( |str| { *str } )
+            .collect::<Vec<&str>>()
+            .join("\n");
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_declaring_error_enum_with_macro() {
+        let source_code = indoc! {"
+        use thiserror::Error;
+
+        #[derive(Error, Debug)]
+        pub enum FormatError {
+            #[error(\"Invalid header (expected {expected:?}, got {found:?})\")]
+            InvalidHeader {
+                expected: String,
+                found: String,
+            },
+            #[error(\"Missing attribute: {0}\")]
+            MissingAttribute(String),
+        }"};
+
+        let result = indoc! {"
+        use thiserror::Error;
+
+        /// [TODO]
+        #[derive(Error, Debug)]
+        pub enum FormatError {
+            #[error(\"Invalid header (expected {expected:?}, got {found:?})\")]
+            InvalidHeader {
+                expected: String,
+                found: String,
+            },
+            #[error(\"Missing attribute: {0}\")]
+            MissingAttribute(String),
+        }"};
+
+        assert_analyzed_source_code(source_code, result)
+    }
+}
