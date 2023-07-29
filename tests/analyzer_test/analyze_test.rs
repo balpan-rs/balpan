89beec475eff98bf3d90b949ad09c42e50b67c6f
not_joon <lbj199874@gmail.com>
2023-07-29 12:30:51 +0900

    Add test for Ignorable insertion annotation cases

--- tests/analyzer_test/analyze_test.rs
+++ tests/analyzer_test/analyze_test.rs
@@ -6,8 +6,8 @@ mod analyze_test {
     use balpan::grammar::{fetch_grammars, build_grammars};
 
     fn assert_analyzed_source_code(source_code: &str, expected: &str) {
-        fetch_grammars();
-        build_grammars(None);
+        fetch_grammars().unwrap();
+        build_grammars(None).unwrap();
 
         let analyzer = Analyzer {
             source_code: source_code.to_string()
@@ -60,6 +60,73 @@ mod analyze_test {
             #[error(\"Missing attribute: {0}\")]
             MissingAttribute(String),
         }"};
+
+        assert_analyzed_source_code(source_code, result)
+    }
+
+    #[test]
+    fn test_stacked_macros() {
+        let source_code = indoc! {"
+            #[derive(Deserialize)]
+            #[serde(bound(deserialize = \"T: Deserialize<'de>\"))]
+            struct List<T> {
+                #[serde(deserialize_with = \"deserialize_vec\")]
+                items: Vec<T>,
+            }"};
+
+        let result = indoc! {"
+            /// [TODO]
+            #[derive(Deserialize)]
+            #[serde(bound(deserialize = \"T: Deserialize<'de>\"))]
+            struct List<T> {
+                #[serde(deserialize_with = \"deserialize_vec\")]
+                items: Vec<T>,
+            }"};
+
+        assert_analyzed_source_code(source_code, result)
+    }
+
+    #[test]
+    #[ignore]
+    fn test_ignore_todo_test_macro() {
+        let source_code = indoc! {"
+            #[cfg(test)]
+            mod tests {
+                use super::*;
+
+                #[test]
+                fn test_foo() {
+                    assert_eq!(foo(), 1);
+                }
+            }"};
+
+        let result = indoc! {"
+            #[cfg(test)]
+            mod tests {
+                use super::*;
+
+                #[test]
+                fn test_foo() {
+                    assert_eq!(foo(), 1);
+                }
+            }"};
+
+        assert_analyzed_source_code(source_code, result)
+    }
+
+    #[test]
+    fn test_ignore_doc_macro() {
+        let source_code = indoc! {"
+            #[doc = \"This is a doc comment\"]
+            fn foo() {
+                println!(\"foo\");
+            }"};
+
+        let result = indoc! {"
+            #[doc = \"This is a doc comment\"]
+            fn foo() {
+                println!(\"foo\");
+            }"};
 
         assert_analyzed_source_code(source_code, result)
     }
