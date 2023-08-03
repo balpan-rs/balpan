72258f4655dd48853cb616e59fcd2b7f13375e4e
Jaeyeol Lee <rijgndqw012@gmail.com>
2023-08-04 02:53:13 +0900

    Refactor integration tests
    Remove duplicated assertion codes
    - Thanks to @ever0de

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -2,4 +2,28 @@
 mod integration_test {
     mod rust_test;
     mod python_test;
+
+    use balpan::analyzer::{Analyzer,Traversable};
+    use balpan::grammar::{fetch_grammars, build_grammars};
+
+    pub fn assert_analyzed_source_code(source_code: &str, expected: &str, language: &str) {
+        fetch_grammars().unwrap();
+        build_grammars(None).unwrap();
+
+        let analyzer = Analyzer {
+            source_code: source_code.to_string(),
+            language: language.to_string(),
+        };
+
+        let writer_queue = &analyzer.analyze();
+        let mut string_vector = vec![];
+
+        for line in writer_queue {
+            string_vector.push(String::from(line));
+        }
+
+        let actual: String = string_vector.join("\n");
+
+        assert_eq!(expected, actual);
+    }
 }
--- tests/integration_test/rust_test/anyhow_case_test.rs
+++ tests/integration_test/rust_test/anyhow_case_test.rs
@@ -1,34 +1,7 @@
 #[cfg(test)]
 mod anyhow_case_test {
     use indoc::indoc;
-    use balpan::analyzer::{Analyzer,Traversable};
-    use balpan::grammar::{fetch_grammars, build_grammars};
-
-    fn assert_analyzed_source_code(source_code: &str, expected: &str, language: &str) {
-        fetch_grammars().unwrap();
-        build_grammars(None).unwrap();
-
-        let analyzer = Analyzer {
-            source_code: source_code.to_string(),
-            language: language.to_string()
-        };
-
-        let writer_queue = &analyzer.analyze();
-        let mut string_vector = vec![];
-
-        for line in writer_queue {
-            string_vector.push(String::from(line));
-        }
-
-
-        let actual: String = string_vector
-            // .iter()
-            // .map( |str| { *str } )
-            // .collect::<Vec<&str>>()
-            .join("\n");
-
-        assert_eq!(expected, actual);
-    }
+    use crate::integration_test::assert_analyzed_source_code;
 
     #[test]
     fn test_declaring_error_enum_with_macro() {
