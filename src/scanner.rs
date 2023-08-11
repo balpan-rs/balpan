9015e921b54b42a69f1b6da605d8752bd7c55e50
Jaeyeol Lee <rijgndqw012@gmail.com>
2023-08-12 01:04:18 +0900

    Introduce `Scanner` struct

---
+++ src/scanner.rs
@@ -0,0 +1,61 @@
+use std::fs::File;
+use std::io::{Read, Write};
+use std::path::Path;
+
+use git2::Repository;
+
+use crate::grammar::{fetch_grammars, build_grammars};
+use crate::utils::list_available_files;
+use crate::analyzer::{Analyzer, Traversable};
+
+
+pub struct Scanner;
+
+impl Scanner {
+    #[inline]
+    pub fn scan(repo: &Repository) {
+        fetch_grammars().unwrap();
+        build_grammars(None).unwrap();
+
+        if let Some(workdir) = repo.workdir() {
+            let repo_root = workdir.to_string_lossy().to_string();
+            let filenames = list_available_files(&repo_root);
+            for filename in filenames {
+                let path = Path::new(&filename);
+                let language = match path.extension() {
+                    Some(os_str) => {
+                        match os_str.to_str() {
+                            Some("rs") => "rust",
+                            Some("py") => "python",
+                            _ => "",
+                        }
+                    },
+                    _ => "",
+                    };
+
+                if let Ok(mut file) = File::open(path) {
+                    let mut source_code = String::new();
+                    file.read_to_string(&mut source_code);
+                    let analyzer = Analyzer {
+                        source_code,
+                        language: language.to_string(),
+                    };
+
+                    let writer_queue = &analyzer.analyze();
+                    let mut lines = vec![];
+
+                    for line in writer_queue {
+                        lines.push(String::from(line));
+                    }
+
+                    file.write_all(lines.join("\n").as_bytes()).unwrap();
+                }
+                
+
+                println!("{:?}", path.extension());
+                println!("{:?}", path.file_name());
+            }
+
+        }
+    }
+}
