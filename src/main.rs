ffd728cad82bf7689a81ac6d3ea48505375e6975
Lee ByeongJun <lbj199874@gmail.com>
2023-08-14 11:59:40 +0900

    add subcommand suggestion (#50)

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use clap::Command;
 use balpan::scanner::Scanner;
 use balpan::utils::get_current_repository;
 use git2::Repository;
+use strsim::levenshtein;
 
 fn git(args: Vec<String>) {
     std::process::Command::new("git")
@@ -25,6 +26,30 @@ fn find_branch<'a>(repository: &Repository, target: &'a str) -> &'a str {
     ""
 }
 
+fn suggest_subcommand(input: &str) -> Option<&'static str> {
+    let subcommands = vec!["init", "reset"];
+
+    let mut closest = None;
+    let mut smallest_distance = usize::MAX;
+
+    const THRESHOLD: usize = 3;
+
+    for subcommand in subcommands {
+        let distance = levenshtein(input, subcommand);
+
+        match distance {
+            0 => return None,
+            1..=THRESHOLD if distance < smallest_distance => {
+                smallest_distance = distance;
+                closest = Some(subcommand);
+            }
+            _ => {}
+        }
+    }
+
+    closest
+}
+
 fn main() {
     let matches = Command::new("balpan")
             .version("0.2.0")
@@ -45,6 +70,16 @@ fn main() {
     let mut main_branch = String::new();
 
     let is_already_setup;
+
+    // verify that the subcommand entered is correct.
+    if let Some(command) = matches.subcommand_name() {
+        if suggest_subcommand(command).is_some() {
+            eprintln!("Did you mean '{}'?", suggest_subcommand(command).unwrap());
+            return;
+        }
+
+        eprintln!("`{}` is an unknown command. Enter `help` to check the list of available commands.", command);
+    }
 
     if matches.subcommand_matches("init").is_some() {
         let repo = get_current_repository().unwrap();
@@ -97,5 +132,23 @@ fn main() {
                 onboarding_branch,
             ])
         }
+    }
+}
+
+
+#[cfg(test)]
+mod main_tests {
+
+    #[test]
+    fn subcommand_suggestion() {
+        use super::suggest_subcommand;
+
+        assert_eq!(suggest_subcommand("innit"), Some("init"));
+        assert_eq!(suggest_subcommand("resett"), Some("reset"));
+        assert_eq!(suggest_subcommand("unknown"), None);
+        assert_eq!(suggest_subcommand("inot"), Some("init"));
+
+        assert_eq!(suggest_subcommand("init"), None);
+        assert_eq!(suggest_subcommand("reset"), None);
     }
 }
